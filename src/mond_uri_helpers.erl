eeec0b1ae520a38108e41c4be691e8c5c4f10d56
benjamin <32769527+benjaminjellis@users.noreply.github.com>
2026-03-31 13:17:39 +0100

    feat: add to result for option (#20)

--- src/mond_uri_helpers.erl
+++ src/mond_uri_helpers.erl
@@ -1,5 +1,5 @@
 -module(mond_uri_helpers).
--export([parse_uri/1]).
+-export([parse_uri/1, parse_query/1, percent_encode/1]).
 
 parse_uri(String) ->
     case uri_string:parse(String) of
@@ -21,6 +21,52 @@ parse_uri(String) ->
                 maps_get_optional(UriMap, fragment)
             }}
     end.
+
+parse_query(Query) ->
+    case uri_string:dissect_query(Query) of
+        {error, _, _} -> {error, unit};
+        Pairs ->
+            {ok, lists:map(fun
+                ({K, true}) -> {pair, K, <<"">>};
+                ({K, V}) -> {pair, K, V}
+            end, Pairs)}
+    end.
+
+percent_encode(Bin) ->
+    percent_encode(Bin, <<>>).
+
+percent_encode(<<>>, Acc) ->
+    Acc;
+percent_encode(<<Byte, Rest/binary>>, Acc) ->
+    case percent_ok(Byte) of
+        true ->
+            percent_encode(Rest, <<Acc/binary, Byte>>);
+        false ->
+            <<Hi:4, Lo:4>> = <<Byte>>,
+            percent_encode(
+                Rest,
+                <<Acc/binary, $%, (dec2hex(Hi)), (dec2hex(Lo))>>
+            )
+    end.
+
+percent_ok($!) -> true;
+percent_ok($$) -> true;
+percent_ok($') -> true;
+percent_ok($() -> true;
+percent_ok($)) -> true;
+percent_ok($*) -> true;
+percent_ok($+) -> true;
+percent_ok($-) -> true;
+percent_ok($.) -> true;
+percent_ok($_) -> true;
+percent_ok($~) -> true;
+percent_ok(C) when $0 =< C, C =< $9 -> true;
+percent_ok(C) when $A =< C, C =< $Z -> true;
+percent_ok(C) when $a =< C, C =< $z -> true;
+percent_ok(_) -> false.
+
+dec2hex(X) when X >= 0, X =< 9 -> X + $0;
+dec2hex(X) when X >= 10, X =< 15 -> X + $A - 10.
 
 maps_get_optional_lowercase(Map, Key) ->
     case maps:find(Key, Map) of
